48dc8dbd911851998439fcae0558c8e265be686e
FV214Hardiel <darthpanther@gmail.com>
2022-09-25 11:14:11 +0300

    reworked player railgun

--- Assets/Scripts/Weapons/Shooting/Player/PlayerRailgun.cs
+++ Assets/Scripts/Weapons/Shooting/Player/PlayerRailgun.cs
@@ -4,44 +4,43 @@ using UnityEngine;
 
 public class PlayerRailgun : PlayerShooting
 {
-    LineRenderer laserLine;
-    
-    Light shotLight;
-    public Light hitLight;
-    Color c;
-
-    Transform muzzle;
-
-    public float shotDurF;
-    WaitForSeconds shotDuration;
-
-    public float chargDurF;
-    WaitForSeconds chargeDuration;
-
-    public float weapRange;
     
 
-    //public float reloadTime;
-    //float nextShot;
+    Transform muzzle; 
+
+    public float weapRange;    
 
     public float delayBetweenShots;
     float remainingDelay;
 
     PlayerInputActions inputActions;
+    float inputValue;
 
-    //WaitForSeconds reloadTime;
+
+    ParticleSystem chargeLight;
+    public float chargeDuration;
+    AudioSource chargeSound;
+
+    public GameObject prefabOfShot;
+
+    AudioSource shotSound;
+
+
 
     void Start()
     {
-        laserLine = GetComponent<LineRenderer>();
+        source = GetComponentInParent<EntityHandler>().gameObject;
+        muzzle = transform.Find("muzzle");
 
         inputActions = new();
-        
-        shotLight = muzzle.GetComponent<Light>();
-        
-        shotDuration = new WaitForSeconds((shotDurF-0.04f)/10f);
-        chargeDuration = new WaitForSeconds(chargDurF / 10f);
-        c = laserLine.material.color;
+        if (!GameHandler.GameIsPaused) inputActions.PlayerTankControl.Enable();
+
+        shotSound = GetComponent<AudioSource>();
+        chargeSound = transform.Find("ChargeSound").GetComponent<AudioSource>();
+
+        chargeLight = transform.Find("muzzleFlash").GetComponent<ParticleSystem>();
+
+        remainingDelay = 0;
     }
 
     
@@ -50,76 +49,176 @@ public class PlayerRailgun : PlayerShooting
 
         if (GameHandler.GameIsPaused) return; //Checking pause
 
-        if (remainingDelay > 0) remainingDelay -= Time.deltaTime; //Decreasing delay timer 
-
-        if (remainingDelay <= 0 && inputActions.PlayerTankControl.Fire.IsPressed())
+        
+        if (remainingDelay > 0) //Decreasing delay timer  
         {
-            remainingDelay = delayBetweenShots;
-            StartCoroutine(ShotEffect());
+            remainingDelay -= Time.deltaTime;
+            return;
+        }
 
+        inputValue = inputActions.PlayerTankControl.Fire.ReadValue<float>();
+
+        if (inputValue > 0) //Shot
+        {
+
+            //Shot(shotVector);
+            StartCoroutine(Shot());
         }
     }
-    
-    
-    private IEnumerator ShotEffect()
+
+    IEnumerator Shot()
     {
-        shotLight.enabled = true;
-        shotLight.intensity = 0f;
-        for (float lI = 0; lI < 9; lI += 1)
-        {
-            shotLight.intensity += 0.3f;
-            yield return chargeDuration;
-        }
+        remainingDelay = delayBetweenShots;
+        chargeSound.Play();
+        chargeLight.Play();
 
-        laserLine.SetPosition(0, muzzle.transform.position);
+        yield return new WaitForSeconds(chargeDuration);
+        chargeSound.Stop();
+        chargeLight.Stop();
+
+        shotSound.Play();
+
         RaycastHit hit;
-
         if (Physics.Raycast(muzzle.position, muzzle.forward, out hit, weapRange))
         {
-            //Debug.Log(hit.point);
-            hitLight.transform.position = hit.point;
-            laserLine.SetPosition(1, hit.point);
-
-            //Health health = hit.collider.GetComponentInParent<Health>();            
-            //if (health != null)
-            //{
-            //    health.TakingDMG(damage, source);
-            //}
-
-            EntityHandler eh = hit.collider.GetComponentInParent<EntityHandler>();
+            Debug.Log(hit.collider);
+            EntityHandler eh = hit.collider.GetComponentInParent<EntityHandler>(false);
             if (eh != null)
             {
-                eh.DealDamage(damage, source);
+                Debug.Log(eh);
+                Debug.Log(source);
+                if (!eh.isDead) //Checking if target is alive and wasnt already hit by this shot
+                {
+                    eh.DealDamage(damage, source);
+                }
             }
+
+            WeaponTrail.Create(prefabOfShot, muzzle.position, hit.point); //Shot VFX if hit
         }
         else
         {
-            //Debug.Log(hit.point);
-            laserLine.SetPosition(1, muzzle.position + weapRange * muzzle.forward);
-            
+            WeaponTrail.Create(prefabOfShot, muzzle.position, muzzle.position + muzzle.forward * weapRange); //Shot VFX if no hit
         }
-        laserLine.enabled = true;
-        c.a = 1;
-        laserLine.material.color = c;
-        hitLight.enabled = true;
-        yield return new WaitForSeconds(0.04f);
-        hitLight.enabled = false;
-        for (float lI = 0; lI < 9; lI += 1)
-        {
-            //hitLight.transform.position = hit.point;
-            shotLight.intensity -= 2f;
-            c.a -= 0.1f;
-            laserLine.material.color = c;
-            yield return shotDuration;
-            
-        }
-        hitLight.enabled = false;
-        laserLine.enabled = false;
-       
-        
-        
 
+        remainingDelay = delayBetweenShots;
     }
+    
 }
 
-   
+
+//LineRenderer laserLine;
+
+//Light shotLight;
+//public Light hitLight;
+//Color c;
+
+//Transform muzzle;
+
+//public float shotDurF;
+//WaitForSeconds shotDuration;
+
+//public float chargDurF;
+//WaitForSeconds chargeDuration;
+
+//public float weapRange;
+
+
+////public float reloadTime;
+////float nextShot;
+
+//public float delayBetweenShots;
+//float remainingDelay;
+
+//PlayerInputActions inputActions;
+
+////WaitForSeconds reloadTime;
+
+//void Start()
+//{
+//    laserLine = GetComponent<LineRenderer>();
+
+//    inputActions = new();
+
+//    shotLight = muzzle.GetComponent<Light>();
+
+//    shotDuration = new WaitForSeconds((shotDurF - 0.04f) / 10f);
+//    chargeDuration = new WaitForSeconds(chargDurF / 10f);
+//    c = laserLine.material.color;
+//}
+
+
+//void Update()
+//{
+
+//    if (GameHandler.GameIsPaused) return; //Checking pause
+
+//    if (remainingDelay > 0) remainingDelay -= Time.deltaTime; //Decreasing delay timer 
+
+//    if (remainingDelay <= 0 && inputActions.PlayerTankControl.Fire.IsPressed())
+//    {
+//        remainingDelay = delayBetweenShots;
+//        StartCoroutine(ShotEffect());
+
+//    }
+//}
+
+
+//private IEnumerator ShotEffect()
+//{
+//    shotLight.enabled = true;
+//    shotLight.intensity = 0f;
+//    for (float lI = 0; lI < 9; lI += 1)
+//    {
+//        shotLight.intensity += 0.3f;
+//        yield return chargeDuration;
+//    }
+
+//    laserLine.SetPosition(0, muzzle.transform.position);
+//    RaycastHit hit;
+
+//    if (Physics.Raycast(muzzle.position, muzzle.forward, out hit, weapRange))
+//    {
+//        //Debug.Log(hit.point);
+//        hitLight.transform.position = hit.point;
+//        laserLine.SetPosition(1, hit.point);
+
+//        //Health health = hit.collider.GetComponentInParent<Health>();            
+//        //if (health != null)
+//        //{
+//        //    health.TakingDMG(damage, source);
+//        //}
+
+//        EntityHandler eh = hit.collider.GetComponentInParent<EntityHandler>();
+//        if (eh != null)
+//        {
+//            eh.DealDamage(damage, source);
+//        }
+//    }
+//    else
+//    {
+//        //Debug.Log(hit.point);
+//        laserLine.SetPosition(1, muzzle.position + weapRange * muzzle.forward);
+
+//    }
+//    laserLine.enabled = true;
+//    c.a = 1;
+//    laserLine.material.color = c;
+//    hitLight.enabled = true;
+//    yield return new WaitForSeconds(0.04f);
+//    hitLight.enabled = false;
+//    for (float lI = 0; lI < 9; lI += 1)
+//    {
+//        //hitLight.transform.position = hit.point;
+//        shotLight.intensity -= 2f;
+//        c.a -= 0.1f;
+//        laserLine.material.color = c;
+//        yield return shotDuration;
+
+//    }
+//    hitLight.enabled = false;
+//    laserLine.enabled = false;
+
+
+
+
+//}
