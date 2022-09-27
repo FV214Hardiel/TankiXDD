f4020e526ea1fdf9ff73cb5ee7f4bc9a25420386
FV214Hardiel <darthpanther@gmail.com>
2022-09-27 22:35:51 +0300

    additional ability ui changes

--- Assets/Scripts/UI/UIAbilityHandler.cs
+++ Assets/Scripts/UI/UIAbilityHandler.cs
@@ -7,13 +7,24 @@ public class UIAbilityHandler : MonoBehaviour
     public List<GameObject> abilitySlots;
     public string test = "Sample Text";
     public List<RectTransform> remCDSlots;
+
+    public int slots;
+    public GameObject slotPrefab;
     // Start is called before the first frame update
     void OnEnable()
     {
-       foreach (AbilitySlot _slot in GetComponentsInChildren<AbilitySlot>())
-        {
-            abilitySlots.Add(_slot.gameObject);
-           
+        //foreach (AbilitySlot _slot in GetComponentsInChildren<AbilitySlot>())
+        // {
+        //     abilitySlots.Add(_slot.gameObject);
+
+        // }
+        slots = GameInfoSaver.instance.chosenAbilities.Count;
+        for (int i = 1; i <= slots; i++)
+            {
+            GameObject newSlot = Instantiate(slotPrefab, transform);
+            abilitySlots.Add(newSlot);
+            remCDSlots.Add(newSlot.transform.Find("RemCD").GetComponent<RectTransform>());
+
         }
         
     }
