c1c3391efe71440746e615f713202da95bfe962c
FV214Hardiel <darthpanther@gmail.com>
2022-11-21 19:09:55 +0300

    basics for levels statistics

---
+++ Assets/Scripts/Levels/LevelStatisticsManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelStatisticsManager : MonoBehaviour
+{
+    public static LevelStatisticsManager instance;
+
+    public Dictionary<string, float> dictOfStats;
+
+    private void OnEnable()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        dictOfStats = new();
+    }
+
+    
+    public void AddValue(string key, float value)
+    {   
+        if (dictOfStats.ContainsKey(key))
+        {
+            dictOfStats[key] += value;
+            
+        }
+        else
+        {
+            dictOfStats.Add(key, value);
+        }
+        print(key + ": " + dictOfStats[key]);
+
+    }
+}
