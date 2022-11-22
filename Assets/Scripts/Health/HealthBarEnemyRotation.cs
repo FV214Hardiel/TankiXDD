0045691361fc74adc60d90c5de40e9833fff05fe
FV214Hardiel <darthpanther@gmail.com>
2022-11-22 19:30:23 +0300

    claring scripts

--- Assets/Scripts/Health/HealthBarEnemyRotation.cs
+++ Assets/Scripts/Health/HealthBarEnemyRotation.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class HealthBarEnemyRotation : MonoBehaviour
 {
     Transform mainCamera;
-    // Start is called before the first frame update
+    
     void Start()
     {
         mainCamera = Camera.main.transform;
 
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
         gameObject.transform.rotation = mainCamera.rotation;
