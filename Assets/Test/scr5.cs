424a8e2fed6afd7e5adb0a2a2a57d9db7ac82fe9
FV214Hardiel <darthpanther@gmail.com>
2022-11-22 19:30:30 +0300

    test

--- Assets/Test/scr5.cs
+++ Assets/Test/scr5.cs
@@ -5,10 +5,19 @@ using UnityEngine.InputSystem;
 
 public class scr5 : MonoBehaviour
 {
-    
+    Ray ray;
+
+    //RaycastHit hit;
+    public LayerMask layers;
 
     void OnEnable()
     {
+        ray = new(transform.position, transform.forward);
+
+        
+        layers |= (1 << LayerMask.NameToLayer("RedTeam"));
+        
+        
         
     }
 
@@ -21,20 +30,50 @@ public class scr5 : MonoBehaviour
     {
         if (context.performed)
         {
+
             print("shot");
 
-            RaycastHit[] hit = Physics.RaycastAll(transform.position, transform.forward, 40f);
+            RaycastHit[] hit = Physics.RaycastAll(ray, 40f, layers);
             
-            foreach (RaycastHit item in hit)
+            List<RaycastHit> newList = new();
+            newList.AddRange(hit);
+
+            //foreach (RaycastHit item in hit)
+            //{
+            //    print(item.transform.name);
+            //}
+
+            Vector3 endOfLine = transform.position + transform.forward * 40f; 
+            newList.Sort((x, y) => x.distance.CompareTo(y.distance));
+            foreach (RaycastHit item in newList)
             {
+                
+
                 print(item.transform.name);
+                if (item.transform.gameObject.layer == LayerMask.NameToLayer("Ground"))
+                {
+                    endOfLine = item.point;
+                    break;
+                }
             }
-            
+
+            Debug.DrawLine(transform.position, endOfLine, Color.blue, 5f);
+
         }
 
-        
-        
 
+
+        //if (context.performed)
+        //{
+        //    print("shot with single rays");
+        //    float range = 40f;
+
+        //    Physics.Raycast(ray, out hit, range);
+            
+        //}
+
+
+        
     }
     
 
