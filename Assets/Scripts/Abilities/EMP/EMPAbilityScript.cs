8449f6d90acc269d374651141da5589604b5671c
FV214Hardiel <darthpanther@gmail.com>
2023-02-03 12:05:09 +0300

    emp ability basis added

---
+++ Assets/Scripts/Abilities/EMP/EMPAbilityScript.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+public class EMPAbilityScript : AbilityBase
+{
+
+    enum AbilityState { ready, cooldown }
+    AbilityState state = AbilityState.ready;
+
+    IEntity source;
+
+    void Start()
+    {
+        GetStats();
+
+        SetIcon();
+
+        source = GetComponent<IEntity>();
+    }
+        
+    void Update()
+    {
+        switch (state)
+        {
+            case AbilityState.ready:
+                if (Input.GetButtonDown(abilityKey))
+                {
+
+                    state = AbilityState.cooldown;
+                    UseAbility();
+                    remainingCooldown = abilityCooldown;
+
+                }
+                break;
+            case AbilityState.cooldown:
+                if (remainingCooldown > 0)
+                {
+                    remainingCooldown -= Time.deltaTime;
+                    abilityDisplay.localScale = new Vector3(1, remainingCooldown / abilityCooldown, 1);
+                }
+                else
+                {
+                    state = AbilityState.ready;
+                }
+                break;
+        }
+    }
+
+    void UseAbility()
+    {
+        print("EMP Radius " + abilityArea);
+    }
+
+}
