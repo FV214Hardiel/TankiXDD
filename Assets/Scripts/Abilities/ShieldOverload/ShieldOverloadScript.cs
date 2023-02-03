5114251e30fb9c352d6b3fdb9909c14351d601f8
FV214Hardiel <darthpanther@gmail.com>
2023-02-03 12:05:40 +0300

    new ability Shield Overload added

---
+++ Assets/Scripts/Abilities/ShieldOverload/ShieldOverloadScript.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldOverloadScript : AbilityBase
+{
+    enum AbilityState { ready, active, cooldown }
+    AbilityState state = AbilityState.ready;
+
+    float remainingDuration;
+    IEntity source;
+
+    Shooting shotScript;
+    void Start()
+    {
+        GetStats();
+
+        SetIcon();
+
+        source = GetComponent<IEntity>();
+
+        shotScript = GetComponentInChildren<Shooting>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (state)
+        {
+            case AbilityState.ready:
+                if (Input.GetButtonDown(abilityKey))
+                {
+
+                    state = AbilityState.active;
+                    UseAbility();
+                    remainingCooldown = abilityCooldown;
+                    remainingDuration = abilityDuration;
+
+                }
+                break;
+            case AbilityState.active:
+                abilityDisplay.localScale = Vector3.one;
+                if (remainingDuration > 0)
+                {
+
+                    remainingDuration -= Time.deltaTime;
+                }
+                else
+                {
+                    DisableEffect();
+                    state = AbilityState.cooldown;
+                    
+                    
+                }
+                break;
+
+            case AbilityState.cooldown:
+                if (remainingCooldown > 0)
+                {
+                    remainingCooldown -= Time.deltaTime;
+                    abilityDisplay.localScale = new Vector3(1, remainingCooldown / abilityCooldown, 1);
+                }
+                else
+                {
+                    state = AbilityState.ready;
+                }
+                break;
+        }
+    }
+
+    void UseAbility()
+    {
+        print("ShieldOverload " + abilityDuration);
+
+        shotScript.outputDamageModifiers += Modifier;
+      //  source.ShieldScript.maxSP
+    }
+
+    void DisableEffect()
+    {
+        shotScript.outputDamageModifiers -= Modifier;
+    }
+
+    float Modifier(float dmg)
+    {
+        float newdmg = dmg + source.ShieldScript.currentSP * 0.05f;
+        source.ShieldScript.ChangeCurrentSP(-10);
+        return newdmg;
+    }
+}
