9e9e18f1a4428b4c11221c1a4ad1b4ab30c6d083
FV214Hardiel <darthpanther@gmail.com>
2023-02-16 13:45:14 +0300

    move and rename rotation bars script

--- Assets/Scripts/UI/Bars/BarsEnemyRotation.cs
+++ Assets/Scripts/UI/Bars/BarsEnemyRotation.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthBarEnemyRotation : MonoBehaviour
+public class BarsEnemyRotation : MonoBehaviour
 {
     Transform mainCamera;
     
