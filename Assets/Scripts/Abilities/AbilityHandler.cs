43145968362f54a66b5f9dce9e68989c68afc8b4
FV214Hardiel <darthpanther@gmail.com>
2023-02-16 11:32:10 +0300

    Update AbilityHandler.cs

--- Assets/Scripts/Abilities/AbilityHandler.cs
+++ Assets/Scripts/Abilities/AbilityHandler.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AbilityHandler : MonoBehaviour
 {
     public List<AbilityCard> abilitiesList;
     UIAbilityHandler abilityUI;
-    void Start()
-    {
-        
-        abilityUI = FindObjectOfType<UIAbilityHandler>();
 
+    void OnEnable()
+    {
+        abilityUI = GameObject.Find("AbilitiesUI").GetComponent<UIAbilityHandler>();
+        abilityUI.enabled = true;
+        
         int slotCount = 0;
-        //Debug.Log(abilitiesList[0]);
+
+        int slots = GameInfoSaver.instance.chosenAbilities.Count;
+
+        for (int i = 1; i <= slots; i++)
+        {
+            GameObject newSlot = Instantiate(abilityUI.slotPrefab, abilityUI.transform);
+            abilityUI.abilitySlots.Add(newSlot);
+            abilityUI.remCDSlots.Add(newSlot.transform.Find("RemCD").GetComponent<RectTransform>());
+
+        }
+        
         foreach (AbilityCard _ability in GameInfoSaver.instance.chosenAbilities)
         {
             if (_ability != null)
@@ -23,7 +35,7 @@ public class AbilityHandler : MonoBehaviour
                 addedAbility.GetType().GetField("abilitySlot").SetValue(addedAbility, slotCount);
                 
                 addedAbility.GetType().GetField("abilityDisplay").SetValue(addedAbility, abilityUI.remCDSlots[slotCount]);
-                
+
                 GetComponent<EntityHandler>().abilities.Add(addedAbility);
 
                 slotCount++;
@@ -33,8 +45,5 @@ public class AbilityHandler : MonoBehaviour
     }
 
     
-    void Update()
-    {
-        
-    }
+    
 }
