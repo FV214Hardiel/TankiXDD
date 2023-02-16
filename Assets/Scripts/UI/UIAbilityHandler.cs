18eeb56f527df134a18c875e448d84da7d68a0e8
FV214Hardiel <darthpanther@gmail.com>
2023-02-16 11:29:45 +0300

    Update UIAbilityHandler.cs

--- Assets/Scripts/UI/UIAbilityHandler.cs
+++ Assets/Scripts/UI/UIAbilityHandler.cs
@@ -10,7 +10,7 @@ public class UIAbilityHandler : MonoBehaviour
 
     public int slots;
     public GameObject slotPrefab;
-    // Start is called before the first frame update
+   
     void OnEnable()
     {
         //foreach (AbilitySlot _slot in GetComponentsInChildren<AbilitySlot>())
@@ -18,14 +18,16 @@ public class UIAbilityHandler : MonoBehaviour
         //     abilitySlots.Add(_slot.gameObject);
 
         // }
-        slots = GameInfoSaver.instance.chosenAbilities.Count;
-        for (int i = 1; i <= slots; i++)
-            {
-            GameObject newSlot = Instantiate(slotPrefab, transform);
-            abilitySlots.Add(newSlot);
-            remCDSlots.Add(newSlot.transform.Find("RemCD").GetComponent<RectTransform>());
+       // print(GameInfoSaver.instance.chosenAbilities.Count);
+        //slots = GameInfoSaver.instance.chosenAbilities.Count;
+        
+        //for (int i = 1; i <= slots; i++)
+        //    {
+        //    GameObject newSlot = Instantiate(slotPrefab, transform);
+        //    abilitySlots.Add(newSlot);
+        //    remCDSlots.Add(newSlot.transform.Find("RemCD").GetComponent<RectTransform>());
 
-        }
+        //}
         
     }
 
