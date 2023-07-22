cb17b24a9a91fd97fbfdfe0d0678525cdfbeaf04
FV214Hardiel <92205042+FV214Hardiel@users.noreply.github.com>
2023-07-22 10:44:47 +0300

    obsolete shield scripts now commented
    will be deleted in future

--- Assets/Scripts/Shield/AIShield.cs
+++ Assets/Scripts/Shield/AIShield.cs
@@ -5,193 +5,193 @@ using UnityEngine.UI;
 
 public class AIShield : Shield
 {
-    Transform damagePopupPrefab;
-    Transform mainCamera;
+    //Transform damagePopupPrefab;
+    //Transform mainCamera;
     
-    Transform barsTransform;
+    //Transform barsTransform;
 
-    CumulativeDamageNumbers popup;
+    //CumulativeDamageNumbers popup;
 
-    float takenDamageSum;
+    //float takenDamageSum;
 
-    [SerializeField] IShieldBar sb;
+    //[SerializeField] IShieldBar sb;
 
-    void OnEnable()
-    {
-        damagePopupPrefab = Resources.Load<Transform>("DamageNumbersPopup");
-        mainCamera = Camera.main.transform;
+    //void OnEnable()
+    //{
+    //    damagePopupPrefab = Resources.Load<Transform>("DamageNumbersPopup");
+    //    mainCamera = Camera.main.transform;
 
-        popup = GetComponentInChildren<CumulativeDamageNumbers>(true);
+    //    popup = GetComponentInChildren<CumulativeDamageNumbers>(true);
 
-        materialPropertyBlock = new();
+    //    materialPropertyBlock = new();
 
-        eh = GetComponent<TankEntity>();
-        eh.ShieldScript = this;
+    //    eh = GetComponent<TankEntity>();
+    //    eh.ShieldScript = this;
 
-        baseSP = eh.hullCard.baseSP;
-        maxSP = baseSP;
-        currentSP = maxSP;
+    //    baseSP = eh.hullCard.baseSP;
+    //    maxSP = baseSP;
+    //    currentSP = maxSP;
 
-        rechargeRate = eh.hullCard.shieldRechargeRate;
-        rechargeDelay = eh.hullCard.shieldRechargeDelay;
+    //    rechargeRate = eh.hullCard.shieldRechargeRate;
+    //    rechargeDelay = eh.hullCard.shieldRechargeDelay;
 
-        //Shield bar
-        sb = GetComponentInChildren<IShieldBar>(true);
-        sb.StartBar();
-        //sb.ConnectShield(this);
-        sb.ChangeMaxSP(maxSP);
-        sb.UpdateBar(currentSP);
+    //    //Shield bar
+    //    sb = GetComponentInChildren<IShieldBar>(true);
+    //    sb.StartBar();
+    //    //sb.ConnectShield(this);
+    //    sb.ChangeMaxSP(maxSP);
+    //    sb.UpdateBar(currentSP);
 
-        barsTransform = transform.Find("floatingBars");
+    //    barsTransform = transform.Find("floatingBars");
 
-        sounds = transform.Find("Sounds");
-        takingHitSound = sounds.Find("TakingHitSoundShield").GetComponent<AudioSource>();
-        takingEMPSound = sounds.Find("TakingEMPSoundShield").GetComponent<AudioSource>();
-        shieldBrokenSound = sounds.Find("ShieldDestroyed").GetComponent<AudioSource>();
-        shieldRechargingSound = sounds.Find("ShieldRegenSound").GetComponent<AudioSource>();
-    }
+    //    sounds = transform.Find("Sounds");
+    //    takingHitSound = sounds.Find("TakingHitSoundShield").GetComponent<AudioSource>();
+    //    takingEMPSound = sounds.Find("TakingEMPSoundShield").GetComponent<AudioSource>();
+    //    shieldBrokenSound = sounds.Find("ShieldDestroyed").GetComponent<AudioSource>();
+    //    shieldRechargingSound = sounds.Find("ShieldRegenSound").GetComponent<AudioSource>();
+    //}
 
-    private void Start()
-    {
-        //meshRenderers = eh.meshRenderers;
-        EnableShieldShader();
-    }
+    //private void Start()
+    //{
+    //    //meshRenderers = eh.meshRenderers;
+    //    EnableShieldShader();
+    //}
     
-    void Update()
-    {       
-        if (eh.outOfDamage > rechargeDelay && !isRecharging && currentSP < maxSP)
-        {
-            StartShieldRecharge();
-        }
+    //void Update()
+    //{       
+    //    if (eh.outOfDamage > rechargeDelay && !isRecharging && currentSP < maxSP)
+    //    {
+    //        StartShieldRecharge();
+    //    }
 
-        if (isRecharging)
-        {
-            currentSP += rechargeRate * Time.deltaTime;
+    //    if (isRecharging)
+    //    {
+    //        currentSP += rechargeRate * Time.deltaTime;
             
-            if (currentSP >= maxSP)
-            {
-                currentSP = maxSP;
-                StopShieldRecharge();
-            }
+    //        if (currentSP >= maxSP)
+    //        {
+    //            currentSP = maxSP;
+    //            StopShieldRecharge();
+    //        }
 
-            sb.UpdateBar(currentSP);
-        }
-    }
+    //        sb.UpdateBar(currentSP);
+    //    }
+    //}
 
   
 
-    public override void TakingDMG(Damage dmgInstance)
-    {
-        takingHitSound.Play();
-        StopShieldRecharge();
+    //public override void TakingDMG(Damage dmgInstance)
+    //{
+    //    takingHitSound.Play();
+    //    StopShieldRecharge();
 
-        if (takenDamageSum == 0)
-        {
-            StartCoroutine(AccumulateDMG());
+    //    if (takenDamageSum == 0)
+    //    {
+    //        StartCoroutine(AccumulateDMG());
 
-        }
-        takenDamageSum += Mathf.Clamp(dmgInstance.damage, 0, currentSP);
+    //    }
+    //    takenDamageSum += Mathf.Clamp(dmgInstance.damage, 0, currentSP);
 
-        PopupAdd(takenDamageSum);
+    //    PopupAdd(takenDamageSum);
 
-        currentSP -= dmgInstance.damage;  
+    //    currentSP -= dmgInstance.damage;  
 
-        if (currentSP <= 0)
-        {
-            shieldBrokenSound.Play();
-            DisableShieldShader();
+    //    if (currentSP <= 0)
+    //    {
+    //        shieldBrokenSound.Play();
+    //        DisableShieldShader();
 
-            eh.HealthScript.OverDamage(new Damage(0 - currentSP, dmgInstance.source));            
+    //        eh.HealthScript.OverDamage(new Damage(0 - currentSP, dmgInstance.source));            
             
-            currentSP = 0;
-        }
+    //        currentSP = 0;
+    //    }
 
-        sb.UpdateBar(currentSP);
+    //    sb.UpdateBar(currentSP);
 
-    }
+    //}
 
-    public override void TakingEMP(Damage dmgInstance)
-    {
-        takingEMPSound.Play();
-        StopShieldRecharge();
+    //public override void TakingEMP(Damage dmgInstance)
+    //{
+    //    takingEMPSound.Play();
+    //    StopShieldRecharge();
 
-        if (takenDamageSum == 0)
-        {
-            StartCoroutine(AccumulateDMG());
-        }
+    //    if (takenDamageSum == 0)
+    //    {
+    //        StartCoroutine(AccumulateDMG());
+    //    }
 
-        PopupAdd(dmgInstance.damage);
+    //    PopupAdd(dmgInstance.damage);
 
-        currentSP -= dmgInstance.damage;
+    //    currentSP -= dmgInstance.damage;
 
-        if (currentSP <= 0)
-        {
-            shieldBrokenSound.Play();
-            DisableShieldShader();
+    //    if (currentSP <= 0)
+    //    {
+    //        shieldBrokenSound.Play();
+    //        DisableShieldShader();
 
-            eh.LoseEMPTenacity(new Damage(0 - currentSP, dmgInstance.source));
+    //        eh.LoseEMPTenacity(new Damage(0 - currentSP, dmgInstance.source));
 
-            currentSP = 0;
-        }
-        sb.UpdateBar(currentSP);
+    //        currentSP = 0;
+    //    }
+    //    sb.UpdateBar(currentSP);
 
 
-    }
+    //}
 
-    public override void ChangeCurrentSP(float change)
-    {
-        base.ChangeCurrentSP(change);
-        sb.UpdateBar(currentSP);
+    //public override void ChangeCurrentSP(float change)
+    //{
+    //    base.ChangeCurrentSP(change);
+    //    sb.UpdateBar(currentSP);
 
-    }
+    //}
 
-    void PopupAdd(float damage)
-    {
+    //void PopupAdd(float damage)
+    //{
 
-        if (popup.gameObject.activeSelf == false)
-        {
-            popup.gameObject.SetActive(true);
-        }
-        popup.AddValue(damage);
-    }
+    //    if (popup.gameObject.activeSelf == false)
+    //    {
+    //        popup.gameObject.SetActive(true);
+    //    }
+    //    popup.AddValue(damage);
+    //}
 
-    IEnumerator AccumulateDMG()
-    {
-        yield return new WaitForEndOfFrame();
+    //IEnumerator AccumulateDMG()
+    //{
+    //    yield return new WaitForEndOfFrame();
 
-        DamageNumbersPopup.CreateStatic(damagePopupPrefab, barsTransform, takenDamageSum, Color.blue);
-        takenDamageSum = 0;
+    //    DamageNumbersPopup.CreateStatic(damagePopupPrefab, barsTransform, takenDamageSum, Color.blue);
+    //    takenDamageSum = 0;
 
-    }
+    //}
 
-    public override void StartShieldRecharge()
-    {
-        isRecharging = true;
+    //public override void StartShieldRecharge()
+    //{
+    //    isRecharging = true;
 
-        EnableShieldShader();
-    }
-    public override void StopShieldRecharge()
-    {
-        isRecharging = false;
+    //    EnableShieldShader();
+    //}
+    //public override void StopShieldRecharge()
+    //{
+    //    isRecharging = false;
 
-    }
+    //}
 
-    public override void EnableShieldShader()
-    {       
-        materialPropertyBlock.SetFloat("_isShieldUp", 1.0f);
-        foreach (MeshRenderer item in eh.meshRenderers)
-        {
-            item.SetPropertyBlock(materialPropertyBlock);
-        }
-    }
+    //public override void EnableShieldShader()
+    //{       
+    //    materialPropertyBlock.SetFloat("_isShieldUp", 1.0f);
+    //    foreach (MeshRenderer item in eh.meshRenderers)
+    //    {
+    //        item.SetPropertyBlock(materialPropertyBlock);
+    //    }
+    //}
 
-    public override void DisableShieldShader()
-    {
-        materialPropertyBlock.SetFloat("_isShieldUp", 0.0f);
-        foreach (MeshRenderer item in eh.meshRenderers)
-        {
-            item.SetPropertyBlock(materialPropertyBlock);
-        }
-    }
+    //public override void DisableShieldShader()
+    //{
+    //    materialPropertyBlock.SetFloat("_isShieldUp", 0.0f);
+    //    foreach (MeshRenderer item in eh.meshRenderers)
+    //    {
+    //        item.SetPropertyBlock(materialPropertyBlock);
+    //    }
+    //}
 
 }
--- Assets/Scripts/Shield/PlayerShield.cs
+++ Assets/Scripts/Shield/PlayerShield.cs
@@ -4,140 +4,140 @@ using UnityEngine;
 public class PlayerShield : Shield
 {
 
-    [SerializeField] IShieldBar sb;
+    //[SerializeField] IShieldBar sb;
 
-    void OnEnable()
-    {
-        eh = GetComponent<TankEntity>();
-        eh.ShieldScript = this;
+    //void OnEnable()
+    //{
+    //    eh = GetComponent<TankEntity>();
+    //    eh.ShieldScript = this;
                 
-        materialPropertyBlock = new();        
+    //    materialPropertyBlock = new();        
 
-        baseSP = eh.hullCard.baseSP;
-        maxSP = baseSP;
-        currentSP = maxSP;        
+    //    baseSP = eh.hullCard.baseSP;
+    //    maxSP = baseSP;
+    //    currentSP = maxSP;        
 
-        //Handling UI ShieldBar
+    //    //Handling UI ShieldBar
         
 
-        rechargeRate = eh.hullCard.shieldRechargeRate;
-        rechargeDelay = eh.hullCard.shieldRechargeDelay;
+    //    rechargeRate = eh.hullCard.shieldRechargeRate;
+    //    rechargeDelay = eh.hullCard.shieldRechargeDelay;
 
-        sounds = transform.Find("Sounds");
-        takingHitSound = sounds.Find("TakingHitSoundShield").GetComponent<AudioSource>();
-        takingEMPSound = sounds.Find("TakingEMPSoundShield").GetComponent<AudioSource>();
-        shieldBrokenSound = sounds.Find("ShieldDestroyed").GetComponent<AudioSource>();
-        shieldRechargingSound = sounds.Find("ShieldRegenSound").GetComponent<AudioSource>();
-    }
+    //    sounds = transform.Find("Sounds");
+    //    takingHitSound = sounds.Find("TakingHitSoundShield").GetComponent<AudioSource>();
+    //    takingEMPSound = sounds.Find("TakingEMPSoundShield").GetComponent<AudioSource>();
+    //    shieldBrokenSound = sounds.Find("ShieldDestroyed").GetComponent<AudioSource>();
+    //    shieldRechargingSound = sounds.Find("ShieldRegenSound").GetComponent<AudioSource>();
+    //}
 
 
-    private void Start()
-    {
-        //meshRenderers = eh.meshRenderers;
-        EnableShieldShader();
-    }
+    //private void Start()
+    //{
+    //    //meshRenderers = eh.meshRenderers;
+    //    EnableShieldShader();
+    //}
     
-    void Update()
-    {
-        if (eh.outOfDamage > rechargeDelay && !isRecharging && currentSP < maxSP)
-        {
-            StartShieldRecharge();
-        }
+    //void Update()
+    //{
+    //    if (eh.outOfDamage > rechargeDelay && !isRecharging && currentSP < maxSP)
+    //    {
+    //        StartShieldRecharge();
+    //    }
 
-        if (isRecharging)
-        {
-            currentSP += rechargeRate * Time.deltaTime; 
+    //    if (isRecharging)
+    //    {
+    //        currentSP += rechargeRate * Time.deltaTime; 
                         
-            if (currentSP >= maxSP)
-            {
-                currentSP = maxSP;
-                StopShieldRecharge();
-            }                        
+    //        if (currentSP >= maxSP)
+    //        {
+    //            currentSP = maxSP;
+    //            StopShieldRecharge();
+    //        }                        
 
-            shieldRechargingSound.pitch = 0.6f + (currentSP / maxSP) * 2;
+    //        shieldRechargingSound.pitch = 0.6f + (currentSP / maxSP) * 2;
 
-            sb.UpdateBar(currentSP);
-        }
-    }
+    //        sb.UpdateBar(currentSP);
+    //    }
+    //}
 
-    public override void TakingDMG(Damage dmgInstance)
-    {
-        takingHitSound.Play();
-        StopShieldRecharge();        
+    //public override void TakingDMG(Damage dmgInstance)
+    //{
+    //    takingHitSound.Play();
+    //    StopShieldRecharge();        
 
-        currentSP -= dmgInstance.damage;
+    //    currentSP -= dmgInstance.damage;
 
-        if (currentSP <= 0)
-        {
-            shieldBrokenSound.Play();
-            DisableShieldShader();
+    //    if (currentSP <= 0)
+    //    {
+    //        shieldBrokenSound.Play();
+    //        DisableShieldShader();
             
-            eh.HealthScript.OverDamage(new Damage(0 - currentSP, dmgInstance.source));
+    //        eh.HealthScript.OverDamage(new Damage(0 - currentSP, dmgInstance.source));
 
-            currentSP = 0;
-        }
+    //        currentSP = 0;
+    //    }
         
-        sb.UpdateBar(currentSP);
+    //    sb.UpdateBar(currentSP);
 
-    }
+    //}
 
-    public override void TakingEMP(Damage dmgInstance)
-    {
-        takingHitSound.Play();
-        StopShieldRecharge();
+    //public override void TakingEMP(Damage dmgInstance)
+    //{
+    //    takingHitSound.Play();
+    //    StopShieldRecharge();
 
-        currentSP -= dmgInstance.damage;
+    //    currentSP -= dmgInstance.damage;
 
-        if (currentSP <= 0)
-        {
-            shieldBrokenSound.Play();
-            DisableShieldShader();
+    //    if (currentSP <= 0)
+    //    {
+    //        shieldBrokenSound.Play();
+    //        DisableShieldShader();
 
-            eh.LoseEMPTenacity(new Damage(0 - currentSP, dmgInstance.source));
+    //        eh.LoseEMPTenacity(new Damage(0 - currentSP, dmgInstance.source));
 
-            currentSP = 0;
-        }
+    //        currentSP = 0;
+    //    }
 
         
 
-    }
+    //}
 
-    public override void ChangeCurrentSP(float change)
-    {
-        base.ChangeCurrentSP(change);
-        sb.UpdateBar(currentSP);
+    //public override void ChangeCurrentSP(float change)
+    //{
+    //    base.ChangeCurrentSP(change);
+    //    sb.UpdateBar(currentSP);
 
-    }
+    //}
 
-    public override void StartShieldRecharge()
-    {
-        isRecharging = true;
-        shieldRechargingSound.Play();
+    //public override void StartShieldRecharge()
+    //{
+    //    isRecharging = true;
+    //    shieldRechargingSound.Play();
 
-        EnableShieldShader();
-    }
-    public override void StopShieldRecharge()
-    {
-        isRecharging = false;
-        shieldRechargingSound.Stop();   
-    }
+    //    EnableShieldShader();
+    //}
+    //public override void StopShieldRecharge()
+    //{
+    //    isRecharging = false;
+    //    shieldRechargingSound.Stop();   
+    //}
 
-    public override void EnableShieldShader() 
-    {     
-        materialPropertyBlock.SetFloat("_isShieldUp", 1.0f);
-        foreach (MeshRenderer item in eh.meshRenderers)
-        {            
-            item.SetPropertyBlock(materialPropertyBlock);
-        }
-    }
+    //public override void EnableShieldShader() 
+    //{     
+    //    materialPropertyBlock.SetFloat("_isShieldUp", 1.0f);
+    //    foreach (MeshRenderer item in eh.meshRenderers)
+    //    {            
+    //        item.SetPropertyBlock(materialPropertyBlock);
+    //    }
+    //}
 
-    public override void DisableShieldShader() 
-    {
-        materialPropertyBlock.SetFloat("_isShieldUp", 0.0f);
-        foreach (MeshRenderer item in eh.meshRenderers)
-        {
-            item.SetPropertyBlock(materialPropertyBlock);
-        }
-    }
+    //public override void DisableShieldShader() 
+    //{
+    //    materialPropertyBlock.SetFloat("_isShieldUp", 0.0f);
+    //    foreach (MeshRenderer item in eh.meshRenderers)
+    //    {
+    //        item.SetPropertyBlock(materialPropertyBlock);
+    //    }
+    //}
 
 }
